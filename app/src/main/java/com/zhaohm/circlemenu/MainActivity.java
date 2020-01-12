cb23d420ca19ccb06df009b4ce1c6c9f50cebf83
ZhaoHongMiao <937412729@qq.com>
2020-01-12 21:39:18 +0800

    实现功能

--- app/src/main/java/com/zhaohm/circlemenu/MainActivity.java
+++ app/src/main/java/com/zhaohm/circlemenu/MainActivity.java
@@ -1,13 +1,25 @@
 package com.zhaohm.circlemenu;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import view.CircleMenuLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private String[] mItemTexts = new String[]{"放大镜 ", "尺子", "分贝测试仪", "手电筒",
+            "计算器", "SOS"};//圆盘菜单显示文字
+    private int[] mItemImgs = new int[]{R.drawable.home_mbank_1_normal,
+            R.drawable.home_mbank_2_normal, R.drawable.home_mbank_3_normal,
+            R.drawable.home_mbank_4_normal, R.drawable.home_mbank_5_normal,
+            R.drawable.home_mbank_6_normal};//圆盘菜单显示图片
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//导入布局
+        //初始化圆盘控件
+        CircleMenuLayout mCircleMenuLayout = (CircleMenuLayout) findViewById(R.id.id_menulayout);
+        //初始化圆盘控件菜单
+        mCircleMenuLayout.setMenuItemIconsAndTexts(mItemImgs, mItemTexts);
     }
 }
